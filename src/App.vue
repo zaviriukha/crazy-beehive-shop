1ca6b26dd10e76793308f51fde5d2f26c784a323
Val <v.zaviriukha@gmail.com>
2019-07-14 22:39:31 +0300

    - test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
+        <span>Vuetify gghhgdyhyh</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
