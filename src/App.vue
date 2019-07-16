2b98e7ca6e66a4eb29519425d93c00b571039c06
Val <v.zaviriukha@gmail.com>
2019-07-17 00:50:08 +0300

    - test

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,66 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify gghhgdyhyh</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-app>
+        <v-toolbar app>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+                <v-text-field placeholder='Search' append-icon='search' style='max-width: 200px'></v-text-field>
+
+                <v-spacer></v-spacer>
+
+                <v-toolbar-items>
+                    <v-btn flat>home</v-btn>
+                    <v-btn flat>shop</v-btn>
+                    <v-btn flat>about</v-btn>
+                </v-toolbar-items>
+
+                <v-img max-width='120' src='https://www.designfreelogoonline.com/wp-content/uploads/2017/01/000789-free-Organic-logo-design-free-food-logo-maker-01.png'></v-img>
+
+                <v-toolbar-items>
+                    <v-btn flat>blog</v-btn>
+                    <v-btn flat>contanct</v-btn>
+                    <v-btn flat>featured</v-btn>
+                </v-toolbar-items>
+
+                <v-spacer></v-spacer>
+
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on }">
+                        <v-btn flat
+                               v-on="on"
+                        >
+                            {{currency}} <v-icon>keyboard_arrow_down</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-list dense>
+                        <v-list-tile v-for="(c, index) in currencies" :key="index" @click="currency = c">
+                            <v-list-tile-title>{{ c }}</v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-menu>
+
+                <v-btn icon>
+                    <v-icon>shopping_cart</v-icon>
+                </v-btn>
+
+        </v-toolbar>
+
+        <router-view/>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+    import HelloWorld from './components/HelloWorld'
+    import MainNav from "./components/mainNav";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+
+    export default {
+        name: 'App',
+        components: {
+            MainNav,
+            HelloWorld
+        },
+        data: () => ({
+            currency: 'USD',
+            currencies: ['USD', 'EUR']
+        })
     }
-  }
-}
 </script>
---
+++ src/components/mainNav.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-text-field placeholder='Search' append-icon='search' style='max-width: 200px'></v-text-field>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn flat>home</v-btn>
+            <v-btn flat>shop</v-btn>
+            <v-btn flat>about</v-btn>
+        </v-toolbar-items>
+
+        <v-img max-width='120' src='https://www.designfreelogoonline.com/wp-content/uploads/2017/01/000789-free-Organic-logo-design-free-food-logo-maker-01.png'></v-img>
+
+        <v-toolbar-items>
+            <v-btn flat>blog</v-btn>
+            <v-btn flat>contanct</v-btn>
+            <v-btn flat>featured</v-btn>
+        </v-toolbar-items>
+
+        <v-spacer></v-spacer>
+
+        <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+                <v-btn flat
+                       v-on="on"
+                >
+                    {{currency}} <v-icon>keyboard_arrow_down</v-icon>
+                </v-btn>
+            </template>
+            <v-list dense>
+                <v-list-tile v-for="(c, index) in currencies" :key="index" @click="currency = c">
+                    <v-list-tile-title>{{ c }}</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-menu>
+
+        <v-btn icon>
+            <v-icon>shopping_cart</v-icon>
+        </v-btn>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "mainNav",
+        data: () => ({
+            currency: 'USD',
+            currencies: ['USD', 'EUR']
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
